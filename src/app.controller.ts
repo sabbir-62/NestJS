0ec4fbcd5a43c856e277ecb680385d16509b6982
Sabbir Hossain <sabbir.hossain.se62@gmail.com>
2024-09-21 22:50:00 +0600

    file upload using multer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus, ParseFilePipeBuilder, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,33 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: './upload/files',
+      filename: (req, file, cb) => {
+        cb(null, file.originalname)
+      }
+    })
+  }))
+  uploadFile(@UploadedFile(
+    new ParseFilePipeBuilder()
+    .addFileTypeValidator({
+      fileType: 'png'
+    })
+    .addMaxSizeValidator({
+      maxSize: 1000
+    })
+    .build({
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY
+    })
+  ) 
+  file: Express.Multer.File){
+    console.log(file)
+    return {
+      message: 'file uploaded successfully'
+    }
   }
 }
